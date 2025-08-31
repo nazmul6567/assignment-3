4ffa1fcc499ab6c3cffe409d686097624a3d0fcf
nazmul6567 <nazmulhassan6567@gmail.com>
2025-08-31 18:10:41 +0600

    done

--- final project.js
+++ final project.js
@@ -53,7 +53,7 @@ var student = { name: 'jhankar', roll: 1014, department: 'cse' };
 
 //write your code here
 
-console.log(cd
+console.log(
   student['name'].concat(student['roll']) +
     '.'.concat(student['department']) +
     '@ph.ac.bd'
